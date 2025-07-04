d047640f3e112d5c44e21c4cab1930a761448b8f
DevHaider1100 <haider.ejaz007@gmail.com>
2025-07-04 16:33:21 +0500

    Added folder structure for cases and drives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CasesComponent } from './features/cases/cases.component';
+import { DrivesComponent } from './features/drives/drives.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CasesComponent,
+    DrivesComponent
   ],
   imports: [
     BrowserModule
