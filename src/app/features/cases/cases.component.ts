43f32ddf497180d78ffd315abbd4ffe3558ac50c
DevHaider1100 <haider.ejaz007@gmail.com>
2025-07-04 22:17:33 +0500

    Restructured the case data details

--- src/app/features/cases/cases.component.ts
+++ src/app/features/cases/cases.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CaseDataService } from './case-data.service';
 
 @Component({
   selector: 'app-cases',
   templateUrl: './cases.component.html',
   styleUrls: ['./cases.component.css']
 })
-export class CasesComponent {
+export class CasesComponent implements OnInit {
+  cases: any[] = [];
 
+  constructor (private caseDataService: CaseDataService) { }
+
+  ngOnInit(): void {
+    this.cases = this.caseDataService.getCases();
+  }
 }
