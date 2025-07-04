a6218ad027691c45b0e2306dcc46b1c2cedd8424
DevHaider1100 <haider.ejaz007@gmail.com>
2025-07-04 16:41:00 +0500

    Added folder structure for hero section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CasesComponent } from './features/cases/cases.component';
 import { DrivesComponent } from './features/drives/drives.component';
+import { HeroComponent } from './features/hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CasesComponent,
-    DrivesComponent
+    DrivesComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule
