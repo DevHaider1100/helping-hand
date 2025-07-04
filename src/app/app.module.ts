5e481556ce044d8ae2bece7a6deaf8af4836abb9
DevHaider1100 <haider.ejaz007@gmail.com>
2025-07-04 19:09:21 +0500

    Added folder structure for portfolio and cta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { CasesComponent } from './features/cases/cases.component';
 import { DrivesComponent } from './features/drives/drives.component';
 import { HeroComponent } from './features/hero/hero.component';
+import { PortfolioComponent } from './features/portfolio/portfolio.component';
+import { CtaComponent } from './features/cta/cta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CasesComponent,
     DrivesComponent,
-    HeroComponent
+    HeroComponent,
+    PortfolioComponent,
+    CtaComponent
   ],
   imports: [
     BrowserModule
